7c2996f7bd1e08c44c1a084d05528ad9a2583ffc
marc <marc@onemarcfifty.com>
2020-09-15 11:16:52 +0200

    fix for NATted VPS environments

--- server/startbond.sh
+++ server/startbond.sh
@@ -46,12 +46,30 @@ ip link set $bondInterface up mtu 1440
 
 # now find the WAN interface
 
+# The initial idea here was to find the interface that has the public IP
+# address. This will not work in a NAT environment, i.e.
+# where the VPS is behind a NAT router and does not have the
+# public address directly.
+
 export OUR_OWN_IP=`sudo -u nobody curl -s ipinfo.io/ip`
 readarray -d " " -t templine <<< $(ip -br addr | grep $OUR_OWN_IP)
 export OUR_WAN_INTERFACE=${templine[0]}
 
+# Fix : If we do not get an interface this way we just use the first 
+# interface with the default route - we check for a minimum length of 3
+# checking for zero length like this 
+# [ -z "$OUR_WAN_INTERFACE" ] && export OUR_WAN_INTERFACE = ip route | grep default | sed s/.*dev\ //g | sed s/\ .*//g
+# does not work because there is a line feed
+# in the variable
+
+if [ ${#OUR_WAN_INTERFACE} -le 2 ]; then
+    echo "WAN Interface not found - was:${OUR_WAN_INTERFACE}:"
+    export OUR_WAN_INTERFACE=`ip route | grep default | sed s/.*dev\ //g | sed s/\ .*//g`
+    echo "WAN Interface is now: $OUR_WAN_INTERFACE"
+fi
+
 # now add the masquerading rules
 
-iptables -A FORWARD -i bond0 -j ACCEPT
-iptables -A FORWARD -o bond0 -j ACCEPT
-iptables -t nat -A POSTROUTING -o $OUR_WAN_INTERFACE -j MASQUERADE
+#iptables -A FORWARD -i bond0 -j ACCEPT
+#iptables -A FORWARD -o bond0 -j ACCEPT
+#iptables -t nat -A POSTROUTING -o $OUR_WAN_INTERFACE -j MASQUERADE
